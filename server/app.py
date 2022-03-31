f2866bc83a650040d53a9f9f8ae1a2b11388344a
Ramzi Latrous <ramzi.latrous@insat.ucar.tn>
2022-03-31 18:23:22 +0100

    working version of both gateway and server missing kerb authentication

--- server/app.py
+++ server/app.py
@@ -7,7 +7,7 @@ from extensions import jwt, db
 
 from resources.login import LoginResource
 from resources.user import RegisterUserResource, UserResource
-from resources.message import MessageListResource, MessageResource
+from resources.message import MessageListResource
 
 def create_app():
     app = Flask(__name__)
@@ -27,7 +27,6 @@ def register_resources(app):
     api.add_resource(RegisterUserResource, '/register')
     api.add_resource(UserResource, '/user')
     api.add_resource(MessageListResource, '/message/<int:user1>/<int:user2>')
-    api.add_resource(MessageResource, '/message')
 
 if __name__ == '__main__':
     app = create_app()
--- server/resources/message.py
+++ server/resources/message.py
@@ -7,15 +7,15 @@ from models.message import Message
 class MessageListResource(Resource):
     def get(self, user1, user2):
         data = Message.get_list_messages(user1, user2)
+        print(data)
         return data, HTTPStatus.OK
 
-class MessageResource(Resource):
-    def post(self):
+    def post(self, user1, user2):
         data = request.get_json()
         message = Message(
-            sent_to = data.get('sentTo'),
-            sent_by = data.get('sentBy'),
-            message = data.get('message')
+            sent_to=user2,
+            sent_by=user1,
+            message=data.get('message')
         )
         message.save()
         return {'message': 'saved message'}, HTTPStatus.CREATED
